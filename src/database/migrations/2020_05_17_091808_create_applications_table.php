b4d935c1fc5728a9f8cb74f963f9b081aee15235
Yan Naing <tannaing@gmail.com>
2020-05-17 16:23:17 +0630

    Add migration and model for Application

---
+++ src/database/migrations/2020_05_17_091808_create_applications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description');
+            $table->string('key', 40);
+            $table->tinyInteger('type');
+            $table->boolean('disabled');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', function() {
+    return sha1(md5(10 . 3) . time() . mt_rand());
+});
