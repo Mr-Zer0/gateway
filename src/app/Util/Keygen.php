8bf69673227c9c8ecb47328cfb11d86866460429
Yan Naing <tannaing@gmail.com>
2020-05-18 19:11:29 +0630

    Add keygen for application api key

---
+++ src/app/Util/Keygen.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Util;
+
+use App\Application;
+use App\User;
+
+/**
+ * This class part of the Gateway project
+ * 
+ * Utility class for application key generate
+ */
+class Keygen
+{
+
+    /**
+     * Return random generated appliation api key based on current micro time, application id and user id
+     *
+     * @param App\Application $app 
+     * @return String
+     **/
+    public static function generate(Application $app, User $user)
+    {
+        return sha1(md5($app->id . $user->id) . time() . mt_rand());
+    }
+}
