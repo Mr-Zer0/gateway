b35702cc9d19db2580dbc6067aa457fe600ffdb6
Yan Naing <tannaing@gmail.com>
2020-05-21 20:41:52 +0630

    Add required return data

--- src/app/Http/Resources/User.php
+++ src/app/Http/Resources/User.php
@@ -14,6 +14,12 @@ class User extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'    => $this->id,
+            'name'  => $this->name,
+            'email' => $this->email,
+            'created_at'    => $this->created_at,
+            'updated_at'    => $this->updated_at
+        ];
     }
 }
